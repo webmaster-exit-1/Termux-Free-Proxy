814b0545ab8e91b54598b255608daeefcdcc0e36
webmaster-exit-1 <mrgofuckyourself@pm.me>
2024-08-07 05:05:31 -0700

    Update Free-Proxy.sh

--- src/Free-Proxy.sh
+++ src/Free-Proxy.sh
@@ -8,6 +8,7 @@ echo '
 ╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝░░░░░░╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░ v1.6
 
              # Developer : mishakorzhik (He1Zen)
+             # Modifier: webmaster-exit-1 (MrGFY)
              # version : Free-Proxy version 1.6
              # Code  : bash, python
 '|lolcat -p 1.7
